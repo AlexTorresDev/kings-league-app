f9aac40523e59103f9428893b6b34abc64c2830e
Alex Torres <alextorressk@gmail.com>
2023-01-07 15:33:06 -0500

    Add colors teams

---
+++ lib/src/core/framework/colors.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+const bgTeams = {
+  'aniquiladores-fc': LinearGradient(
+    begin: Alignment.centerLeft,
+    end: Alignment.centerRight,
+    stops: [0.1, 0.5, 0.9],
+    colors: [
+      Color(0xFF821F34),
+      Color(0xFF991E38),
+      Color(0xFFF35975),
+    ],
+  ),
+  '1k': LinearGradient(
+    begin: Alignment.centerLeft,
+    end: Alignment.centerRight,
+    stops: [0.1, 0.5, 0.9],
+    colors: [
+      Color(0xFF3DB9CA),
+      Color(0xFF6D6AF3),
+      Color(0xFF8964FE),
+    ],
+  ),
+  'el-barrio': LinearGradient(
+    begin: Alignment.centerLeft,
+    end: Alignment.centerRight,
+    stops: [0.1, 0.5, 0.9],
+    colors: [
+      Color(0xFF142020),
+      Color(0xFF3B949F),
+      Color(0xFF4BA7A2),
+    ],
+  ),
+  'jijantes-fc': LinearGradient(
+    begin: Alignment.centerLeft,
+    end: Alignment.centerRight,
+    stops: [0.1, 0.5, 0.9],
+    colors: [
+      Color(0xFF32385A),
+      Color(0xFF862944),
+      Color(0xFFD23649),
+    ],
+  ),
+  'kunisports': LinearGradient(
+    begin: Alignment.centerLeft,
+    end: Alignment.centerRight,
+    stops: [0.1, 0.5, 0.9],
+    colors: [
+      Color(0xFF101920),
+      Color(0xFF644A55),
+      Color(0xFFA96775),
+    ],
+  ),
+  'los-troncos-fc': LinearGradient(
+    begin: Alignment.centerLeft,
+    end: Alignment.centerRight,
+    stops: [0.1, 0.5, 0.9],
+    colors: [
+      Color(0xFF2D3939),
+      Color(0xFF31595B),
+      Color(0xFF3E6465),
+    ],
+  ),
+  'pio-fc': LinearGradient(
+    begin: Alignment.bottomLeft,
+    end: Alignment.topRight,
+    stops: [0.0, 0.7, 1.0],
+    colors: [
+      Color.fromARGB(255, 148, 138, 16),
+      Color.fromARGB(255, 221, 211, 91),
+      Color.fromARGB(255, 254, 246, 139),
+    ],
+  ),
+  'porcinos-fc': LinearGradient(
+    begin: Alignment.centerLeft,
+    end: Alignment.centerRight,
+    stops: [0.1, 0.5, 0.9],
+    colors: [
+      Color(0xFFD81955),
+      Color(0xFFE44479),
+      Color(0xFFEE7291),
+    ],
+  ),
+  'rayo-barcelona': LinearGradient(
+    begin: Alignment.centerLeft,
+    end: Alignment.centerRight,
+    stops: [0.1, 0.5, 0.9],
+    colors: [
+      Color(0xFFAD7A2A),
+      Color(0xFFC7903B),
+      Color(0xFFF3B047),
+    ],
+  ),
+  'saiyans-fc': LinearGradient(
+    begin: Alignment.centerLeft,
+    end: Alignment.centerRight,
+    stops: [0.1, 0.5, 0.9],
+    colors: [
+      Color(0xFF131A34),
+      Color(0xFF0426A2),
+      Color(0xFF1C42CF),
+    ],
+  ),
+  'ultimate-mostoles': LinearGradient(
+    begin: Alignment.centerLeft,
+    end: Alignment.centerRight,
+    stops: [0.1, 0.5, 0.9],
+    colors: [
+      Color(0xFF141414),
+      Color(0xFF141414),
+      Color(0xFF868686),
+    ],
+  ),
+  'xbuyer-team': LinearGradient(
+    begin: Alignment.centerLeft,
+    end: Alignment.centerRight,
+    stops: [0.1, 0.5, 0.9],
+    colors: [
+      Color(0xFF202020),
+      Color(0xFF515151),
+      Color(0xFF848484),
+    ],
+  ),
+};
