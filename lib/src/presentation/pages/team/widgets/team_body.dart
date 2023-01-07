ac7644197cd81c17b3638b70fae60f4aa2536d59
Alex Torres <alextorressk@gmail.com>
2023-01-07 17:33:13 -0500

    Add grid of players

---
+++ lib/src/presentation/pages/team/widgets/team_body.dart
@@ -0,0 +1,93 @@
+import 'package:kings_league_app/src/config/common.dart';
+import 'package:kings_league_app/src/domain/entities/entities.dart';
+
+class TeamBody extends StatelessWidget {
+  const TeamBody({
+    Key? key,
+    required this.team,
+  }) : super(key: key);
+
+  final Team team;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Column(
+      children: [
+        Text(
+          'Jugadores',
+          style: theme.textTheme.headlineSmall?.copyWith(
+            fontWeight: FontWeight.w600,
+            color: theme.colorScheme.onPrimary,
+          ),
+        ),
+        const SizedBox(height: 24.0),
+        GridView.builder(
+          primary: false,
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 36.0,
+            childAspectRatio: 0.72,
+          ),
+          itemCount: team.players.length,
+          itemBuilder: (_, index) {
+            final player = team.players[index];
+
+            return Column(
+              children: [
+                Image.network(
+                  'https://kingsleague.dev/teams/players/${player.image}',
+                  height: 120,
+                  loadingBuilder: (_, child, loadingProgress) {
+                    if (loadingProgress == null) {
+                      return child;
+                    } else {
+                      return const SizedBox(
+                        height: 120.0,
+                      );
+                    }
+                  },
+                ),
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 8.0,
+                    vertical: 4.0,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.black45,
+                    borderRadius: BorderRadius.circular(4.0),
+                  ),
+                  child: Column(
+                    children: [
+                      Text(
+                        player.name,
+                        textAlign: TextAlign.center,
+                        style: theme.textTheme.bodyMedium?.copyWith(
+                          color: theme.colorScheme.onPrimary,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      const SizedBox(height: 3.0),
+                      Text(
+                        player.role.toUpperCase(),
+                        textAlign: TextAlign.center,
+                        style: theme.textTheme.bodySmall?.copyWith(
+                          color: theme.colorScheme.primary,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
